8085b88e40178b3aa937bb7012e2340a6a2d51b6
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-12-03 00:45:00 -0300

    Add part 2

--- 2020/day-1.ts
+++ 2020/day-1.ts
@@ -1,22 +1,37 @@
 import { fetchInput } from './shared/input';
 
-function findPair(sum: number, list: number[]): [number, number] {
+interface Solution {
+  part1: number;
+  part2: number;
+}
+
+function findSolution(sum: number, list: number[]): Solution {
+  let part1 = 0;
+  let part2 = 0;
+
   for (const i of list) {
     for (const j of list) {
       if (i + j === sum) {
-        return [i, j];
+        part1 = i * j;
+      }
+
+      for (const k of list) {
+        if (i + j + k === sum) {
+          part2 = i * j * k;
+        }
       }
     }
   }
 
-  throw new Error('No match.');
+  return { part1, part2 };
 }
 
 const input = fetchInput(1);
 
-const [a, b] = findPair(
+const { part1, part2 } = findSolution(
   2020,
   input.split('\n').map((i) => parseInt(i, 10))
 );
 
-console.log(a * b);
+console.log('Part 1:', part1);
+console.log('Part 2:', part2);
